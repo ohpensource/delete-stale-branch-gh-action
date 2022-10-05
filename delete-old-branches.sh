cbf16002ed9d42b688a228b7d337590e8fec274b
Peter Valachovic <pvalachovic@ohpen.com>
2022-10-05 13:12:50 +0200

    feat: CCDESK-292516 reworking - reworking grep 6

--- delete-old-branches.sh
+++ delete-old-branches.sh
@@ -54,9 +54,9 @@ extra_branch_or_tag_protected() {
     local br=${1} ref="${2}"
     echo ${br}
     if [[ "${ref}" == "branch" ]]; then
-        echo "${br}" | grep -qE $EXCLUDE_BRANCH_REGEX
+        echo "${br}" | grep -Eiw "${EXCLUDE_BRANCH_REGEX}"
     elif [[ "${ref}" == "tag" ]]; then
-        echo "${br}" | grep -qE $EXCLUDE_TAG_REGEX
+        echo "${br}" | grep -Eiw "${EXCLUDE_TAG_REGEX}"
     fi
 
     return $?
